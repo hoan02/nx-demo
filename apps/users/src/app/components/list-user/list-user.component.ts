e7534f176f1db754fbe7c76c0da224ffe3244918
HoanIT <lehoan.dev@gmail.com>
2025-02-06 10:11:28 +0700

    [chore]: add material UI

--- apps/users/src/app/components/list-user/list-user.component.ts
+++ apps/users/src/app/components/list-user/list-user.component.ts
@@ -1,17 +1,45 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { IUser } from '@nx-demo/libs';
 import { ToastrService } from 'ngx-toastr';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatSort } from '@angular/material/sort';
+
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { IUser, IUserRole } from '@nx-demo/libs';
 import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-list-user',
-  imports: [CommonModule],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatSortModule,
+    MatSelectModule,
+    FormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+  ],
   templateUrl: './list-user.component.html',
 })
-export class ListUserComponent implements OnInit {
-  users: IUser[] = [];
+export class ListUserComponent implements OnInit, AfterViewInit {
+  userList = new MatTableDataSource<IUser>([]);
+  displayedColumns: string[] = [
+    'position',
+    'name',
+    'email',
+    'fullName',
+    'role',
+    'actions',
+  ];
+  roles: string[] = Object.values(IUserRole);
+  selectedRole = '';
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(
     private router: Router,
@@ -24,11 +52,26 @@ export class ListUserComponent implements OnInit {
     this.loadUsers();
   }
 
+  ngAfterViewInit(): void {
+    this.userList.paginator = this.paginator;
+    this.userList.sort = this.sort;
+  }
+
+  getPosition(index: number): number {
+    return index + 1 + this.paginator.pageIndex * this.paginator.pageSize;
+  }
+
   loadUsers(): void {
     this.userService.getUsers().subscribe({
-      next: (data) => (this.users = data),
+      next: (data) => {
+        this.userList.data = data;
+      },
       error: (err) => console.error('Error loading users', err),
     });
+  }
+
+  applyFilter(): void {
+    this.userList.filter = this.selectedRole.trim().toLowerCase();
   }
 
   onAddUser(): void {
